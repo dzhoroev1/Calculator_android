e67e580f4f36cb7a3f99f6b3fd36bdf383316d3b
dzhoroev1gmail.com <dzhoroev1@gmail.com>
2020-11-20 18:53:48 +0900

    [few fixes to detect button and change textView]

--- app/src/main/java/com/example/calculator_practice/MainActivity.java
+++ app/src/main/java/com/example/calculator_practice/MainActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    private CustomView viewText;
+    private TextView textView;
+    private CustomView  customView;
     private Button one;
     private Button two;
     private Button three;
@@ -23,12 +27,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = (TextView) findViewById(R.id.textView);
         one = (Button) findViewById(R.id.one);
         two = (Button) findViewById(R.id.two);
+
+        //works well color changing
+//        one.setBackgroundColor(getResources().getColor(R.color.black));
 
         one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                textView.setText("Hello 1");
             }
         });
     }
